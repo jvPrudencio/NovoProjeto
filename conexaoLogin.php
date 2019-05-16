daa2c2ab61c78a510194db52b2fec48e6070a747
laboratorio <laboratorio@EP2463.academico.projecao>
2019-05-16 08:04:17 -0300

    inicio do projeto

--- conexaoLogin.php
+++ conexaoLogin.php
@@ -1,8 +1,18 @@
 <?php
+session_start();
+include 'conexaoCadastro.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$email=$_POST['email'];
+$senha=$_POST['senha'];
 
+$login = "SELECT * FROM projeto WHERE email= '$email' AND senha= '$senha'";
+$result= $cone->query($login);
+
+if($email->num_rows>0){
+    $_SESSION['email']='email';
+    $_SESSION['senha']='senha';
+    header('location:PrimeiraPagina.php');
+}
+else{
+    echo alert("Dados invalidos");
+}
