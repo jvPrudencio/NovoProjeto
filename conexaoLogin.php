d91718060da6de2776a3c8c13329e36490a4c536
laboratorio <laboratorio@EP2463.academico.projecao>
2019-05-16 08:09:54 -0300

    inicio do projeto

--- conexaoLogin.php
+++ conexaoLogin.php
@@ -8,9 +8,9 @@ $senha=$_POST['senha'];
 $login = "SELECT * FROM projeto WHERE email= '$email' AND senha= '$senha'";
 $result= $cone->query($login);
 
-if($email->num_rows>0){
-    $_SESSION['email']='email';
-    $_SESSION['senha']='senha';
+if($result->num_rows>0){
+    $_SESSION['email']=$email;
+    $_SESSION['senha']=$senha;
     header('location:PrimeiraPagina.php');
 }
 else{
