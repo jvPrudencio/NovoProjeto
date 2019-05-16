7adf816aa927a371d63cc23bb9914443c5b99751
laboratorio <laboratorio@EP2463.academico.projecao>
2019-05-16 08:14:37 -0300

    inicio do projeto

--- conexaoLogin.php
+++ conexaoLogin.php
@@ -2,15 +2,15 @@
 session_start();
 include 'conexaoCadastro.php';
 
-$email=$_POST['email'];
-$senha=$_POST['senha'];
+$email=$_POST['email_login'];
+$senha=$_POST['senha_login'];
 
 $login = "SELECT * FROM projeto WHERE email= '$email' AND senha= '$senha'";
 $result= $cone->query($login);
 
 if($result->num_rows>0){
-    $_SESSION['email']=$email;
-    $_SESSION['senha']=$senha;
+    $_SESSION['email_login']=$email;
+    $_SESSION['senha_login']=$senha;
     header('location:PrimeiraPagina.php');
 }
 else{
